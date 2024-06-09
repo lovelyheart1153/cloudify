e990562bb54c8bc39fbd9bda6bed83007dda1867
unknown <genesisserver1153@gmail.com>
2024-06-09 09:16:38 +0100

    Build the navigation bar

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
 const collapsibles = document.querySelectorAll(".collapsible");
+const hamburger = document.querySelector(".hamburger");
+const nav = document.querySelector(".nav");
+
 collapsibles.forEach((item) =>
   item.addEventListener("click", function () {
     this.classList.toggle("collapsible--expanded");
   })
 );
+
+hamburger.addEventListener("click", () => {
+  nav.classList.toggle("nav--open");
+  hamburger.classList.toggle("hamburger--open");
+});
